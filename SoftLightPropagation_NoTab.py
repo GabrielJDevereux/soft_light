a72e2ddeeecb76862011aa52b4beea538e4208ff
GabrielJDevereux <102922938+GabrielJDevereux@users.noreply.github.com>
2022-04-04 09:02:22 +0100

    Add files via upload

---
+++ SoftLightPropagation_NoTab.py
@@ -0,0 +1,58 @@
+import math
+
+flec_x = int(input("Reflector Height (in meter): ")) # insert width
+flec_y = int(input("Reflector Width (in meter): ")) # insert height
+
+c = 7
+m = flec_x*c          # number of squares in the x-direction (adjusted)
+n = flec_y*c           # number of squares in the y-direction (adjusted)
+
+Lux_initial = float(input("Source luminance (in lux): ")) # insert the luminance of the source at the reflector
+L_source = Lux_initial*flec_x*flec_y   # luminous flux
+
+max = int(input("Maximum distance from the reflector in meters: "))
+
+L_reflector = 0.735*L_source  # reflectance value of 73.5%
+
+L_0 = 2*L_reflector/(m*n)
+""""
+L_0 is the luminance of a single point source; factor 2 is there because point source emits light in all directions,
+not just in one side of a plane (as is the case with a sheet reflector)
+"""
+
+
+a = flec_x/m          # side length of a single square ADJUSTED
+
+coordinates = []  # coordinates of a center of each square
+
+# creates a list of all coordinates ADJUSTED
+for i in range(0,m):
+    x = -flec_x/2 + a/2 + i*a
+    for j in range(0,n):
+        y = -flec_y/2 + a/2 + j*a
+        coordinates.append([x,y])
+
+L_z = []  # luminance at various points
+
+# computes luminance at point z
+for z in range(1,max + 1):
+    L_final = 0  # luminance at point z
+    for i in range(0, m*n):
+        x = coordinates[i][0]   # x coordinate
+        y = coordinates[i][1]   # y coordinate
+        r2 = x ** 2 + y ** 2 + z ** 2 # square of a distance between the z point and point (x,y)
+        L = L_0/(4*math.pi*r2)   # inverse square law
+        L_final += L   # summing contributions from all point sources
+    L_z.append(L_final)
+
+# final value is rounded to 3 decimal points
+
+print('Distance (m)', 'Luminance (lux)')
+for z in range(1,max + 1):
+    print(" ",z,"            ",round(L_z[z-1]))
+
+#By Gabriel Devereux
+#Digital Imaging Technician
+#gabjol@me.com | gabrieldevereux.com
+
+
---
+++ SoftnessOfLightCalc_NoTab.py
@@ -0,0 +1,38 @@
+
+import math
+
+flec_x = int(input("Reflector Height (in meter): ")) # insert height
+flec_y = int(input("Reflector Width (in meter): ")) # insert width
+meters = int(input("Maximum distance from the reflector in meters:")) # Distance
+
+def triangle_angle(b_o, a):
+    b = b_o / 2
+    alpha_degrees = math.atan(a/b) * 180 / math.pi   #Calculating angle reflector outward
+    
+    beta_degrees = math.atan(b/a) * 180 / math.pi   * 2 #Calculating point of farthest conversion
+
+    return(beta_degrees)
+
+def soft_angle(angle):
+    large_radius = ((1+math.sin(math.radians(angle/2))) / (math.sin(math.radians(angle/2)))) * 0.08 #Large Radius of theoretical circle
+    tangent = math.sqrt(large_radius**2-2*large_radius*0.08) #the distance from apex to tangent
+
+    hypo_apex = tangent**2 + 0.08**2 #calculating the hypotenuse of the apex (pythag)
+    hypo_apex = math.sqrt(hypo_apex) #pythag2
+
+    innercirc = math.asin(tangent/hypo_apex) * 180 / math.pi #The angle of triangle in circle
+    soft_light_cover = (180-innercirc) * 2 #The angle of light hitting sphere
+
+    return(soft_light_cover)
+    
+print('Distance (m)', '  Verticle Cover (deg)  ', 'Horizontal Cover (deg)')
+
+for i in range(0, meters):
+    print("       ",i+1,"        ",round(soft_angle(triangle_angle(flec_x, i+1)),4),"            ",round(soft_angle(triangle_angle(flec_y, i+1)),4))
+
+print("")
+
+
+#Gabriel Devereux
+#DIT, gabrieldevereux.com
+#gabjol@me.com
